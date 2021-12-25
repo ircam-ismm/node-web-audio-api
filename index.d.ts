47f75b1d23edb7343369d8fb771e8c975248720f
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2021-12-25 14:57:29 +0100

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+export class ExternalObject<T> {
+  readonly '': {
+    readonly '': unique symbol
+    [K: symbol]: T
+  }
+}
+export function plus100(input: number): number
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![deny(clippy::all)]
+
+use napi_derive::napi;
+
+#[cfg(all(
+  any(windows, unix),
+  target_arch = "x86_64",
+  not(target_env = "musl"),
+  not(debug_assertions)
+))]
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[napi]
+pub fn plus_100(input: u32) -> u32 {
+  input + 100
+}
