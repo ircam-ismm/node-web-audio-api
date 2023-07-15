ec28162d005d556f220c76a17ae454f5d9b5f8ca
b-ma <benjamin.matuszewski@gmail.com>
2023-07-15 07:39:58 +0200

    fix: implement channel configuration on destination

--- src/audio_destination_node.rs
+++ src/audio_destination_node.rs
@@ -1,20 +1,31 @@
-use napi::{
-    CallContext, Env, JsFunction, JsObject, JsString, JsUndefined, Property, PropertyAttributes,
-    Result,
-};
+// @TODO
+// This should be generated as any other AudioNode
+
+use crate::*;
+use napi::*;
 use napi_derive::js_function;
-
-use web_audio_api::context::BaseAudioContext;
-use web_audio_api::node::AudioDestinationNode;
-
-use crate::audio_context::NapiAudioContext;
-use crate::offline_audio_context::NapiOfflineAudioContext;
+use web_audio_api::context::*;
+use web_audio_api::node::*;
 
 pub struct NapiAudioDestinationNode(AudioDestinationNode);
 
 impl NapiAudioDestinationNode {
     pub fn create_js_class(env: &Env) -> Result<JsFunction> {
-        env.define_class("AudioDestination", constructor, &[])
+        env.define_class(
+            "AudioDestination",
+            constructor,
+            &[
+                Property::new("channelCount")?
+                    .with_getter(get_channel_count)
+                    .with_setter(set_channel_count),
+                Property::new("channelCountMode")?
+                    .with_getter(get_channel_count_mode)
+                    .with_setter(set_channel_count_mode),
+                Property::new("channelInterpretation")?
+                    .with_getter(get_channel_interpretation)
+                    .with_setter(set_channel_interpretation),
+            ],
+        )
     }
 
     pub fn unwrap(&self) -> &AudioDestinationNode {
@@ -43,6 +54,8 @@ fn constructor(ctx: CallContext) -> Result<JsUndefined> {
     let audio_context_utf8_name = audio_context_name.into_utf8()?.into_owned()?;
     let audio_context_str = &audio_context_utf8_name[..];
 
+    // @note
+    // this seems to be the only difference with other audio nodes
     let native_node = match audio_context_str {
         "AudioContext" => {
             let napi_audio_context = ctx.env.unwrap::<NapiAudioContext>(&js_audio_context)?;
@@ -61,6 +74,101 @@ fn constructor(ctx: CallContext) -> Result<JsUndefined> {
 
     let napi_node = NapiAudioDestinationNode(native_node);
     ctx.env.wrap(&mut js_this, napi_node)?;
+
+    ctx.env.get_undefined()
+}
+
+// -------------------------------------------------
+// AudioNode Interface
+// -------------------------------------------------
+
+#[js_function]
+fn get_channel_count(ctx: CallContext) -> Result<JsNumber> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let channel_count = node.channel_count() as f64;
+
+    ctx.env.create_double(channel_count)
+}
+
+#[js_function(1)]
+fn set_channel_count(ctx: CallContext) -> Result<JsUndefined> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let channel_count = ctx.get::<JsNumber>(0)?.get_double()? as usize;
+    node.set_channel_count(channel_count);
+
+    ctx.env.get_undefined()
+}
+
+#[js_function]
+fn get_channel_count_mode(ctx: CallContext) -> Result<JsString> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let value = node.channel_count_mode();
+    let value_str = match value {
+        ChannelCountMode::Max => "max",
+        ChannelCountMode::ClampedMax => "clamped-max",
+        ChannelCountMode::Explicit => "explicit",
+    };
+
+    ctx.env.create_string(value_str)
+}
+
+#[js_function(1)]
+fn set_channel_count_mode(ctx: CallContext) -> Result<JsUndefined> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let js_str = ctx.get::<JsString>(0)?;
+    let uf8_str = js_str.into_utf8()?.into_owned()?;
+    let value = match uf8_str.as_str() {
+        "max" => ChannelCountMode::Max,
+        "clamped-max" => ChannelCountMode::ClampedMax,
+        "explicit" => ChannelCountMode::Explicit,
+        _ => panic!("undefined value for ChannelCountMode"),
+    };
+    node.set_channel_count_mode(value);
+
+    ctx.env.get_undefined()
+}
+
+#[js_function]
+fn get_channel_interpretation(ctx: CallContext) -> Result<JsString> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let value = node.channel_interpretation();
+    let value_str = match value {
+        ChannelInterpretation::Speakers => "speakers",
+        ChannelInterpretation::Discrete => "discrete",
+    };
+
+    ctx.env.create_string(value_str)
+}
+
+#[js_function(1)]
+fn set_channel_interpretation(ctx: CallContext) -> Result<JsUndefined> {
+    let js_this = ctx.this_unchecked::<JsObject>();
+    let napi_node = ctx.env.unwrap::<NapiAudioDestinationNode>(&js_this)?;
+    let node = napi_node.unwrap();
+
+    let js_str = ctx.get::<JsString>(0)?;
+    let uf8_str = js_str.into_utf8()?.into_owned()?;
+    let value = match uf8_str.as_str() {
+        "speakers" => ChannelInterpretation::Speakers,
+        "discrete" => ChannelInterpretation::Discrete,
+        _ => panic!("undefined value for ChannelInterpretation"),
+    };
+    node.set_channel_interpretation(value);
 
     ctx.env.get_undefined()
 }
