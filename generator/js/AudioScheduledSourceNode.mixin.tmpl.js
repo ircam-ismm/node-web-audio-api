367f26d4232da0b2d0c8f22abce3210e3a5045d4
b-ma <benjamin.matuszewski@gmail.com>
2024-01-07 07:34:12 +0100

    chore: move AudioScheduledSourceNode to own template

---
+++ generator/js/AudioScheduledSourceNode.mixin.tmpl.js
@@ -0,0 +1,49 @@
+const { throwSanitizedError } = require('./lib/errors.js');
+
+const { AudioParam, kNativeAudioParam } = require('./AudioParam.js');
+
+module.exports = (superclass) => {
+  class ${d.name(d.node)} extends superclass {
+    constructor(...args) {
+      super(...args);
+    }
+    // getters
+${d.attributes(d.node).map(attr => {
+  return `
+    get ${d.name(attr)}() {
+      return super.${d.name(attr)};
+    }
+`}).join('')}
+    // setters
+${d.attributes(d.node).filter(attr => !attr.readonly).map(attr => {
+  return `
+    set ${d.name(attr)}(value) {
+      try {
+        super.${d.name(attr)} = value;
+      } catch (err) {
+        throwSanitizedError(err);
+      }
+    }
+`}).join('')}
+    // methods - start / stop
+    ${d.methods(d.node, false).reduce((acc, method) => {
+      // dedup method names
+      if (!acc.find(i => d.name(i) === d.name(method))) {
+        acc.push(method)
+      }
+      return acc;
+    }, []).map(method => {
+  return `
+    ${d.name(method)}(...args) {
+      try {
+        return super.${d.name(method)}(...args);
+      } catch (err) {
+        throwSanitizedError(err);
+      }
+    }
+`}).join('')}
+  }
+
+  return ${d.name(d.node)};
+};
+
