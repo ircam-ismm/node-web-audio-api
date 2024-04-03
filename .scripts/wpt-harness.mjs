b314624486e9285e7535fb48ca059d66d3c01f67
b-ma <benjamin.matuszewski@gmail.com>
2024-04-03 19:04:20 +0200

    cleaning

--- .scripts/wpt-harness.mjs
+++ .scripts/wpt-harness.mjs
@@ -124,7 +124,7 @@ const reporter = {
   },
   pass: message => {
     numPass += 1;
-    // console.log(chalk.dim(indent(chalk.green("√ ") + message, INDENT_SIZE)));
+    console.log(chalk.dim(indent(chalk.green("√ ") + message, INDENT_SIZE)));
   },
   fail: message => {
     if (/threw "[^\"]*Error" instead of/.test(message)) {
