3bd7467f5c3683e17ad0e3699eae103cd5047673
b-ma <benjamin.matuszewski@gmail.com>
2024-04-11 18:13:24 +0200

    clippy

--- generator/rs/lib.tmpl.rs
+++ generator/rs/lib.tmpl.rs
@@ -4,6 +4,7 @@ use napi::{Env, JsObject, Result};
 use napi_derive::module_exports;
 
 // cf. https://users.rust-lang.org/t/vec-f32-to-u8/21522/7
+#[allow(clippy::needless_lifetimes)]
 pub(crate) fn to_byte_slice<'a>(floats: &'a [f32]) -> &'a [u8] {
     unsafe {
         std::slice::from_raw_parts(floats.as_ptr() as *const _, floats.len() * 4)
