44c3bdfffb4a42504d51c248d7003f74ee8f7a2d
b-ma <benjamin.matuszewski@gmail.com>
2024-04-15 10:54:47 +0200

    feat: implement all AudioNode connect / disconnect alternatives

--- generator/js/AudioNode.tmpl.js
+++ generator/js/AudioNode.tmpl.js
@@ -116,7 +116,6 @@ ${d.attributes(d.node).filter(attr => d.name(attr) !== 'context').map(attr => {
   // connect / disconnect
   // ------------------------------------------------------
 
-  // @todo
   // AudioNode connect (AudioNode destinationNode,
   //                    optional unsigned long output = 0,
   //                    optional unsigned long input = 0);
@@ -131,23 +130,61 @@ ${d.attributes(d.node).filter(attr => d.name(attr) !== 'context').map(attr => {
       throw new TypeError(\`Failed to execute 'connect' on 'AudioNode': 1 argument required, but only \${arguments.length} present\`);
     }
 
-    const jsDest = args[0];
+    let destination;
+    let output;
+    let input;
 
     // note that audio listener params are not wrapped
-    if (args[0] instanceof AudioParam) {
-      args[0] = args[0][kNapiObj];
-    }
-
     if (args[0] instanceof AudioNode) {
-      args[0] = args[0][kNapiObj];
+      destination = args[0][kNapiObj];
+
+      if (args[1] !== undefined) {
+        output = conversions['unsigned long'](args[1], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+      } else {
+        output = 0;
+      }
+
+      if (args[2] !== undefined) {
+        input = conversions['unsigned long'](args[2], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+      } else {
+        input = 0;
+      }
+    } else if (args[0] instanceof AudioParam) {
+      if (arguments.length > 2) {
+        throw new TypeError("Failed to execute 'connect' on 'AudioNode': parameter 1 is not of type 'AudioNode'");
+      }
+
+      destination = args[0][kNapiObj];
+
+      if (args[1] !== undefined) {
+        output = conversions['unsigned long'](args[1], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+      } else {
+        output = 0;
+      }
+
+      // Rust does not make difference between AudioNode and AudioParam
+      input = 0;
+    } else {
+      throw new TypeError("Failed to execute 'connect' on 'AudioNode': Overload resolution failed");
     }
 
     try {
-      this[kNapiObj].connect(...args);
-      return jsDest;
+      this[kNapiObj].connect(destination, output, input);
     } catch (err) {
       throwSanitizedError(err);
     }
+
+    // return given destination
+    return args[0];
   }
 
   // @todo
@@ -161,21 +198,83 @@ ${d.attributes(d.node).filter(attr => d.name(attr) !== 'context').map(attr => {
   // undefined disconnect (AudioParam destinationParam);
   // undefined disconnect (AudioParam destinationParam, unsigned long output);
 
+  // disconnect
+  // disconnect_dest
+  // disconnect_output
+  // disconnect_dest_from_output
+  // disconnect_dest_from_output_to_input
+
   disconnect(...args) {
     if (!(this instanceof AudioNode)) {
       throw new TypeError("Invalid Invocation: Value of 'this' must be of type 'AudioNode'");
     }
 
-    if (args[0] instanceof AudioParam) {
-      args[0] = args[0][kNapiObj];
+    if (arguments.length > 2) {
+      if (args[0] instanceof AudioNode) {
+        const destination = args[0][kNapiObj];
+        const output = conversions['unsigned long'](args[1], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+        const input = conversions['unsigned long'](args[2], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+
+        try {
+          return this[kNapiObj].disconnect(destination, output, input);
+        } catch (err) {
+          throwSanitizedError(err);
+        }
+      } else {
+        throw new TypeError("Failed to execute 'disconnect' on 'AudioNode': : Overload resolution failed");
+      }
+    } else if (arguments.length === 2) {
+      if (args[0] instanceof AudioNode || args[0] instanceof AudioParam) {
+        const destination = args[0][kNapiObj];
+        const output = conversions['unsigned long'](args[1], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+
+        try {
+          return this[kNapiObj].disconnect(destination, output);
+        } catch (err) {
+          throwSanitizedError(err);
+        }
+      } else {
+        throw new TypeError("Failed to execute 'disconnect' on 'AudioNode': : Overload resolution failed");
+      }
+    } else if (arguments.length === 1) {
+      if (args[0] instanceof AudioNode || args[0] instanceof AudioParam) {
+        const destination = args[0][kNapiObj];
+
+        try {
+          return this[kNapiObj].disconnect(destination);
+        } catch (err) {
+          throwSanitizedError(err);
+        }
+      } else if (Number.isFinite(args[0])) {
+        const output = conversions['unsigned long'](args[0], {
+          enforceRange: true,
+          context: "Failed to execute 'connect' on 'AudioNode':",
+        });
+
+        try {
+          return this[kNapiObj].disconnect(output);
+        } catch (err) {
+          throwSanitizedError(err);
+        }
+      }
+
+      // Note that we don't have the "overload resolution failed" branch here
+      // which seems to be aligned with browsers behavior
     }
 
-    if (args[0] instanceof AudioNode) {
-      args[0] = args[0][kNapiObj];
-    }
-
+    // Just call disconnect for remaning cases
+    // - i.e. including node.disconnect(NaN), node.disconnect(null), etc.
     try {
-      this[kNapiObj].disconnect(...args);
+      this[kNapiObj].disconnect();
     } catch (err) {
       throwSanitizedError(err);
     }
