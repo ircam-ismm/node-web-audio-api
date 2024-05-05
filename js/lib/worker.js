53380b32fe87237e222b6877f405bd368c48682b
b-ma <benjamin.matuszewski@gmail.com>
2024-05-05 10:12:22 +0200

    delete unsused file

--- js/lib/worker.js
+++
@@ -1,9 +0,0 @@
-const { isMainThread } = require('node:worker_threads');
-const { runAudioWorklet, registerProcessor } = require('./../../index.js');
-
-console.log('Inside Worker!');
-console.log('Is main thread', isMainThread);  // Prints 'false'.
-
-class AudioWorkletProcessor { }
-
-runAudioWorklet();
