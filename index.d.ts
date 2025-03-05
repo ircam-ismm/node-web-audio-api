f3c8456cdd4d420533deb1e83642f718b2f5ee88
Michalis Zabaras <mikezaby@gmail.com>
2025-03-05 09:09:26 +0200

    Add missing export types and re-arrange them

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,24 @@
 // Referencing the default lib web api typings
 /// <reference lib="dom" />
-
 declare module "node-web-audio-api" {
+  export import OfflineAudioCompletionEvent = globalThis.OfflineAudioCompletionEvent;
+  export import AudioProcessingEvent = globalThis.AudioProcessingEvent;
+
+  export import BaseAudioContext = globalThis.BaseAudioContext;
   export import AudioContext = globalThis.AudioContext;
   export import OfflineAudioContext = globalThis.OfflineAudioContext;
-  export import AudioBuffer = globalThis.AudioBuffer;
+
+  export import AudioNode = globalThis.AudioNode;
+  export import AudioScheduledSourceNode = globalThis.AudioScheduledSourceNode;
+  export import AudioParam = globalThis.AudioParam;
+  export import AudioDestinationNode = globalThis.AudioDestinationNode;
+  export import AudioListener = globalThis.AudioListener;
+  export import AudioWorklet = globalThis.AudioWorklet;
+  export import AudioParamMap = globalThis.AudioParamMap;
+
   export import PeriodicWave = globalThis.PeriodicWave;
-  export import MediaStreamAudioSourceNode = globalThis.MediaStreamAudioSourceNode;
+  export import AudioBuffer = globalThis.AudioBuffer;
+  export import AudioWorkletNode = globalThis.AudioWorkletNode;
   export import AnalyserNode = globalThis.AnalyserNode;
   export import AudioBufferSourceNode = globalThis.AudioBufferSourceNode;
   export import BiquadFilterNode = globalThis.BiquadFilterNode;
@@ -18,6 +30,7 @@ declare module "node-web-audio-api" {
   export import DynamicsCompressorNode = globalThis.DynamicsCompressorNode;
   export import GainNode = globalThis.GainNode;
   export import IIRFilterNode = globalThis.IIRFilterNode;
+  export import MediaStreamAudioSourceNode = globalThis.MediaStreamAudioSourceNode;
   export import OscillatorNode = globalThis.OscillatorNode;
   export import PannerNode = globalThis.PannerNode;
   export import StereoPannerNode = globalThis.StereoPannerNode;
