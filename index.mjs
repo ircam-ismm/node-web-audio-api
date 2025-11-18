6bbd8703d480c935636c2d7b896049f794b3f03a
b-ma <benjamin.matuszewski@gmail.com>
2025-11-18 10:03:04 +0100

    chore: rewrite exports properly

--- index.mjs
+++ index.mjs
@@ -23,53 +23,49 @@
 import {
   createRequire,
 } from 'module';
+
 const require = createRequire(import.meta.url);
-
 const nativeModule = require('./index.cjs');
-export const {
-  // events
-  OfflineAudioCompletionEvent,
-  AudioProcessingEvent,
-  AudioRenderCapacityEvent,
+// events
+export const OfflineAudioCompletionEvent = nativeModule.OfflineAudioCompletionEvent;
+export const AudioProcessingEvent = nativeModule.AudioProcessingEvent;
+export const AudioRenderCapacityEvent = nativeModule.AudioRenderCapacityEvent;
+// manually written nodes
+export const BaseAudioContext = nativeModule.BaseAudioContext;
+export const AudioContext = nativeModule.AudioContext;
+export const OfflineAudioContext = nativeModule.OfflineAudioContext;
 
-  // manually written nodes
-  BaseAudioContext,
-  AudioContext,
-  OfflineAudioContext,
+export const AudioNode = nativeModule.AudioNode;
+export const AudioScheduledSourceNode = nativeModule.AudioScheduledSourceNode;
+export const AudioParam = nativeModule.AudioParam;
+export const AudioDestinationNode = nativeModule.AudioDestinationNode;
+export const AudioListener = nativeModule.AudioListener;
+export const AudioWorklet = nativeModule.AudioWorklet;
+export const AudioParamMap = nativeModule.AudioParamMap;
+export const AudioRenderCapacity = nativeModule.AudioRenderCapacity;
 
-  AudioNode,
-  AudioScheduledSourceNode,
-  AudioParam,
-  AudioDestinationNode,
-  AudioListener,
-  AudioWorklet,
-  AudioParamMap,
-  AudioRenderCapacity,
-
-  PeriodicWave,
-  AudioBuffer,
-  // generated nodes
-  ScriptProcessorNode,
-  AudioWorkletNode,
-  AnalyserNode,
-  AudioBufferSourceNode,
-  BiquadFilterNode,
-  ChannelMergerNode,
-  ChannelSplitterNode,
-  ConstantSourceNode,
-  ConvolverNode,
-  DelayNode,
-  DynamicsCompressorNode,
-  GainNode,
-  IIRFilterNode,
-  MediaStreamAudioSourceNode,
-  OscillatorNode,
-  PannerNode,
-  StereoPannerNode,
-  WaveShaperNode,
-
-  // helper methods
-  mediaDevices,
-} = nativeModule;
+export const PeriodicWave = nativeModule.PeriodicWave;
+export const AudioBuffer = nativeModule.AudioBuffer;
+// generated nodes
+export const ScriptProcessorNode = nativeModule.ScriptProcessorNode;
+export const AudioWorkletNode = nativeModule.AudioWorkletNode;
+export const AnalyserNode = nativeModule.AnalyserNode;
+export const AudioBufferSourceNode = nativeModule.AudioBufferSourceNode;
+export const BiquadFilterNode = nativeModule.BiquadFilterNode;
+export const ChannelMergerNode = nativeModule.ChannelMergerNode;
+export const ChannelSplitterNode = nativeModule.ChannelSplitterNode;
+export const ConstantSourceNode = nativeModule.ConstantSourceNode;
+export const ConvolverNode = nativeModule.ConvolverNode;
+export const DelayNode = nativeModule.DelayNode;
+export const DynamicsCompressorNode = nativeModule.DynamicsCompressorNode;
+export const GainNode = nativeModule.GainNode;
+export const IIRFilterNode = nativeModule.IIRFilterNode;
+export const MediaStreamAudioSourceNode = nativeModule.MediaStreamAudioSourceNode;
+export const OscillatorNode = nativeModule.OscillatorNode;
+export const PannerNode = nativeModule.PannerNode;
+export const StereoPannerNode = nativeModule.StereoPannerNode;
+export const WaveShaperNode = nativeModule.WaveShaperNode;
+// helper methods
+export const mediaDevices = nativeModule.mediaDevices;
 
 export default nativeModule;
