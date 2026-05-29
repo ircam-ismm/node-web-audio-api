ced37af2f126e716ccac8a735e4bbfda93fdbb58
b-ma <benjamin.matuszewski@gmail.com>
2026-05-29 17:53:13 +0200

    wpt: more robust fetch mock

--- .scripts/wpt-mock/fetch.js
+++ .scripts/wpt-mock/fetch.js
@@ -2,26 +2,32 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-export default function createFetch(basePath) {
+import caller from 'caller';
+
+// some are in wpt/resources
+// some are relative to caller site (cf. audiobuffersource-multi-channel)
+export default function createFetch(wptPath) {
   return function fetch(pathname) {
-    pathname = path.join(basePath, pathname);
+    const callerSite = new URL(caller(1));
+    const relPathname = path.dirname(callerSite.pathname)
+    let buffer;
 
-    return new Promise(resolve => {
-      if (!fs.existsSync(pathname)) {
-        resolve({
-          ok: false,
-          msg: `file ${pathname} not found`,
-        });
-      } else {
-        const buffer = fs.readFileSync(pathname);
+    if (fs.existsSync(path.join(wptPath, pathname))) {
+      buffer = fs.readFileSync(path.join(wptPath, pathname));
+    } else if (fs.existsSync(path.join(wptPath, relPathname, pathname))) {
+      buffer = fs.readFileSync(path.join(wptPath, relPathname, pathname));
+    } else {
+      return Promise.resolve({
+        ok: false,
+        msg: `file ${pathname} not found`,
+      });
+    }
 
-        resolve({
-          ok: true,
-          text: () => buffer.toString(),
-          json: () => JSON.parse(buffer.toString()),
-          arrayBuffer: () => buffer.buffer,
-        });
-      }
+    return Promise.resolve({
+      ok: true,
+      text: () => buffer.toString(),
+      json: () => JSON.parse(buffer.toString()),
+      arrayBuffer: () => buffer.buffer,
     });
   }
 };
